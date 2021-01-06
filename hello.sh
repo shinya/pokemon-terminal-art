9d6762dcb660ed7cb026680b8431e2933b208e14
shinya <pemomomo@gmail.com>
2021-01-06 19:14:46 +0900

    Simplified random number generation

--- hello.sh
+++ hello.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 BASE_URL="https://raw.githubusercontent.com/shinya/pokemon-terminal-art/main/256color/gold/@.txt"
-NUMBER=$(seq -f %03g 1 151 | sort -R | head -n 1)
+NUMBER=$(echo $((RANDOM%151+1)) | printf "%03d" $(cat))
 
 echo ${BASE_URL} | sed -e "s/@/${NUMBER}/g" | xargs -n1 curl -s 
